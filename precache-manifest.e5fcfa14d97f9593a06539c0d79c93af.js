60aa249add192b8c43e03003a8d58c1382460d77
Soobin Kwon <soobkwon@uw.edu>
2018-11-16 16:51:24 -0800

    Updates

---
+++ precache-manifest.e5fcfa14d97f9593a06539c0d79c93af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d375a25b3f2fe4dd26bf",
+    "url": "/ps-7-soobkwon/static/js/runtime~main.d375a25b.js"
+  },
+  {
+    "revision": "ffe46d299308816474f0",
+    "url": "/ps-7-soobkwon/static/js/main.ffe46d29.chunk.js"
+  },
+  {
+    "revision": "1bd0b74a89848cdda644",
+    "url": "/ps-7-soobkwon/static/js/1.1bd0b74a.chunk.js"
+  },
+  {
+    "revision": "ffe46d299308816474f0",
+    "url": "/ps-7-soobkwon/static/css/main.5fe6f138.chunk.css"
+  },
+  {
+    "revision": "1bd0b74a89848cdda644",
+    "url": "/ps-7-soobkwon/static/css/1.e1c8a766.chunk.css"
+  },
+  {
+    "revision": "83f0d6ef7c352aef89fa294daab1f668",
+    "url": "/ps-7-soobkwon/index.html"
+  }
+];
